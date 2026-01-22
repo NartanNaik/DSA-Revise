cc423360f5804e217f7b58f304e1a390eb1cac71
NartanNaik <2024lcs_nartanmarutinaik_c@nie.ac.in>
2026-01-22 15:45:53 +0530

    22-01-2026

---
+++ Array/23_Next_permutation.cpp
@@ -0,0 +1,79 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// you have given an array and you have to first give the second permituation of it that means when you take a permutation os a array in asscending order, you have to look for the original array in that permutation and and have to return the next permutation of it and if the next permutation won't exist becuase it's appeared in the last permutation you have to return the first one
+
+// ex: a = {3,1,2} so it's permutation will be : 123, 132,213,231,312,321 so you have to return 321 and if the array was = {3,2,1} you have to return 123
+
+vector<int> BruteforceSolution(vector<int> &a)
+{
+    // find all the permutations
+    // traverse and search which one is the array elements
+    // return it's next permutation
+
+    // we have to do it with the recursion and this will have lots of TC
+}
+
+vector<int> BetterSolution(vector<int> &a)
+{
+    // this is the inbuild cpp stl how this gonna work we will see in the optimal solution
+    next_permutation(a.begin(), a.end());
+    return a;
+}
+
+vector<int> OptimalSolution(vector<int> &a)
+{
+    // TC = O(3N) in verse case where O(N) is first loop O(N) is second loop and O(N) is reverse at the last in worst case we may have to traverse the entire array
+    // SC = O(1)=> we are not using any extra space
+
+    // 1. find the number which is a[i] < a[i+1]
+    // 2. find the number which is > than it and also smaller one in the array
+    // 2. arrange the remaining elements in sorted order
+    int index = -1;
+    int n = a.size();
+
+    // traverse from the second last element to the starting of the array considering whether the previous one is smaller or not. when we get the first smaller element we store it as index and break the loop
+    for (int i = n - 2; i >= 0; i--)
+    {
+        if (a[i] < a[i + 1])
+        {
+            index = i;
+            break;
+        }
+    }
+
+    // if the index is still the same as before that means we don't have smaller element in the array which's next is bigger than so that means the array it self the last array so we just need to reverse it to get the next permutaion that meant the first permutation
+    if (index == -1)
+    {
+        reverse(a.begin(), a.end());
+        return a;
+    }
+
+    // if we get the index traverse from the last element to the index's element ans search for the element which's greater than the index. in the very movement we get the number which is bigger than the index we have swap the index value and that value and break the array
+    for (int i = n - 1; i >= index; i--)
+    {
+        if (a[i] > a[index])
+        {
+            swap(a[i], a[index]);
+            break;
+        }
+    }
+
+    // after that reverse the array from the index's next element to the last of the array to get the next permutation of it...
+    reverse(a.begin() + index + 1, a.end());
+
+    // return the next permutation array
+    return a;
+}
+
+int main()
+{
+    vector<int> a = {3, 1, 2};
+    vector<int> ans = BetterSolution(a);
+    cout << "Next permutation: ";
+    for (auto it : ans)
+    {
+        cout << it << " ";
+    }
+    return 0;
+}
---
+++ Array/24_Longest_consequtive.cpp
@@ -0,0 +1,151 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// consequtive numbers meant the sequence numbers withought breaking the order example in the array a = {45, 46, 42, 1, 2, 3, 5, 4}; the consiqutives will be 1,2,3,4,5 and 42 and 45,46. so in this question you have to find longest consequtive which in 1,2,3,4,5 in our case and return the length of it
+
+// this will search whether the perticular element is there or not in the array
+bool LenearSearch(vector<int> &a, int num)
+{
+    for (int i = 0; i < a.size(); i++)
+    {
+        if (a[i] == num)
+        {
+            return true;
+        }
+    }
+    return false;
+}
+
+int BruteforceSolution(vector<int> &a)
+{
+    // TC = O(2N)
+    // SC = O(1)
+
+    // atleast one element exist in the array so we take the longest as 1
+    int longest = 1;
+
+    // traverse through the array
+    for (int i = 0; i < a.size(); i++)
+    {
+        // take one element
+        int x = a[i];
+
+        // keep count as one becuase atleast one element exist in the array
+        int cnt = 1;
+
+        // while it's next element don't appear in the array check for the whole array and check whether it's next element exist or not
+        while (LenearSearch(a, x + 1) == true)
+        {
+            // upgrading presenet element to it's next element this will updated to the x of the while loop condition whenever the condition turns true
+            x = x + 1;
+
+            // increase the count if you get it's next element
+            cnt++;
+        }
+
+        // update the longest based on what you got
+        longest = max(longest, cnt);
+    }
+
+    // return the longest
+    return longest;
+}
+
+int BetterSolution(vector<int> &a)
+{
+    // TC = O(NlogN) + O(N)
+    // SC = O(1)
+
+    // sort the array first
+    sort(a.begin(), a.end());
+    int logest = 1;
+    int cnt = 0;
+
+    // take a number to compare with the element in the array whether it's previuos exist or not because if it's preious exist that means the the numbers are in sequence
+    int lastSmaller = INT_MIN;
+
+    // traverse through the entire array
+    for (int i = 0; i < a.size(); i++)
+    {
+        // if the previous of this element is exist already
+        if (a[i] - 1 == lastSmaller)
+        {
+            // increase the count
+            cnt++;
+            // and now keep this item as the previous
+            lastSmaller = a[i];
+        }
+
+        // if the previos of it's not there
+        else if (lastSmaller != a[i])
+        {
+            // keep the the cnt as 1
+            cnt = 1;
+            // and keep it as the previous number of it
+            lastSmaller = a[i];
+        }
+
+        // choose the longest
+        logest = max(logest, cnt);
+    }
+
+    // return the longest
+    return logest;
+}
+
+int OptimalSolution(vector<int> &a)
+{
+    // TC = O(2N+N) => O(3N) where O(2N) for while loop because we are nearly iterating twice for each element, like when we get first element we chack for whether it's next elements are present or not in the array and O(N) for forLoop
+    // SC = O(N) because we are storing the element
+
+    // return the array if the array size is 0
+    if (a.size() == 0)
+    {
+        return 0;
+    }
+    int longest = 1;
+
+    // this will store the all the elements as it is
+    unordered_set<int> st;
+    for (int i = 0; i < a.size(); i++)
+    {
+        st.insert(a[i]);
+    }
+
+    // iterate in the elements stack which we stored
+    for (auto it : st)
+    {
+        // and if the element's preious not present in the stack (that means this is the first element for the consequtives)
+        if (st.find(it - 1) == st.end())
+        {
+            // make count as 1 and keep that item as x
+            int cnt = 1;
+            int x = it;
+
+            // now while the next elements for x exist in the stack
+            while (st.find(x + 1) != st.end())
+            {
+                // make the x as x's next element
+                x = x + 1;
+                // increase the count
+                cnt++;
+            }
+
+            // get the longest
+            longest = max(longest, cnt);
+        }
+    }
+
+    // return the longest
+    return longest;
+}
+
+int main()
+{
+    vector<int> a = {45, 46, 42, 1, 2, 5, 4};
+    // int length = BruteforceSolution(a);
+    // int length = BetterSolution(a);
+    int length = OptimalSolution(a);
+    cout << "The longest consequtive array's length is: " << length;
+    return 0;
+}
